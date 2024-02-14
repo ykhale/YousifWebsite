2dc87019372ec0b4a9fd63b1d3994101ac220c3e
Yousif Khaleel <ykhale2@uic.edu>
2024-02-13 23:26:12 -0600

    Added Pong

---
+++ pong.js
@@ -0,0 +1,127 @@
+const canvas = document.getElementById('pongCanvas');
+const ctx = canvas.getContext('2d');
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 10,
+    velocityX: 5,
+    velocityY: 5,
+    speed: 7,
+    color: 'WHITE'
+};
+
+const user = {
+    x: 0,
+    y: (canvas.height - 100) / 2,
+    width: 10,
+    height: 100,
+    score: 0,
+    color: 'WHITE'
+};
+
+const com = {
+    x: canvas.width - 10,
+    y: (canvas.height - 100) / 2,
+    width: 10,
+    height: 100,
+    score: 0,
+    color: 'WHITE'
+};
+
+function drawRect(x, y, w, h, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+function drawArc(x, y, r, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawScore() {
+    ctx.fillStyle = 'WHITE';
+    ctx.font = '35px sans-serif';
+    ctx.fillText(user.score, canvas.width / 4, canvas.height / 6);
+    ctx.fillText(com.score, 3 * canvas.width / 4, canvas.height / 6);
+}
+
+canvas.addEventListener('mousemove', getMousePos);
+
+function getMousePos(evt) {
+    let rect = canvas.getBoundingClientRect();
+    user.y = evt.clientY - rect.top - user.height / 2;
+}
+
+function resetBall() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.velocityX = -ball.velocityX;
+    ball.speed = 7;
+}
+
+function update() {
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+        ball.velocityY = -ball.velocityY;
+    }
+
+    let player = (ball.x < canvas.width / 2) ? user : com;
+
+    if (collision(ball, player)) {
+        let collidePoint = ball.y - (player.y + player.height / 2);
+        collidePoint = collidePoint / (player.height / 2);
+
+        let angleRad = collidePoint * Math.PI / 4;
+        let direction = (ball.x < canvas.width / 2) ? 1 : -1;
+        ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+        ball.velocityY = ball.speed * Math.sin(angleRad);
+
+        ball.speed += 0.1;
+    }
+
+    if (ball.x - ball.radius < 0) {
+        com.score++;
+        resetBall();
+    } else if (ball.x + ball.radius > canvas.width) {
+        user.score++;
+        resetBall();
+    }
+
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+
+    com.y += ((ball.y - (com.y + com.height / 2))) * 0.1;
+}
+
+function collision(b, p) {
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius;
+
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
+
+    return b.right > p.left && b.top < p.bottom && b.left < p.right && b.bottom > p.top;
+}
+
+function render() {
+    drawRect(0, 0, canvas.width, canvas.height, '#000');
+    drawRect(user.x, user.y, user.width, user.height, user.color);
+    drawRect(com.x, com.y, com.width, com.height, com.color);
+    drawArc(ball.x, ball.y, ball.radius, ball.color);
+    drawScore();
+}
+
+function game() {
+    update();
+    render();
+}
+
+let framePerSecond = 50;
+setInterval(game, 1000 / framePerSecond);
